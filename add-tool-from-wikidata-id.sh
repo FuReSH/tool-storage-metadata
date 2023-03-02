71c95c01bd84e7cad24aa06d43b8bd13513824bb
Till Grallert <tillgrallert@users.noreply.github.com>
2023-03-02 22:45:27 +0100

    added check of input for formally valid Wikidata IDs

--- add-tool-from-wikidata-id.sh
+++ add-tool-from-wikidata-id.sh
@@ -9,40 +9,47 @@ output_dir=$input_dir
 # ask for input
 read -p "Please provide a valid Wikidata ID: " input
 # check if the input is a QId
-
-# empty variable for result of check
-exists=FALSE
-# read all yaml files in the input directory and check if one contains the Wikidata ID
-for fullpath in $input_dir/*\.y*ml; do
-    # only read files if the Wikidata ID has not yet been found
-    if [[ $exists == FALSE ]]; then
-        # read yaml with the yq tool
-        wd_id="$(yq '.wikidata-id' $fullpath)"
-        # if the QID is already present in our directory, switch the value of $exists to TRUE
-        if [[ $wd_id == $input ]]; then
-            exists=TRUE
+if [[ $input =~ (Q[0-9]+) ]]; then
+    wd_id_input=$(echo "$input" | perl -pe 's/^.*(Q\d+).*$/$1/g')
+    echo "Input contains the Wikidata ID $wd_id_input"
+    # empty variable for result of check
+    exists=FALSE
+    # read all yaml files in the input directory and check if one contains the Wikidata ID
+    for fullpath in $input_dir/*\.y*ml; do
+        # only read files if the Wikidata ID has not yet been found
+        if [[ $exists == FALSE ]]; then
+            # read yaml with the yq tool
+            wd_id="$(yq '.wikidata-id' $fullpath)"
+            # if the QID is already present in our directory, switch the value of $exists to TRUE
+            if [[ $wd_id == $wd_id_input ]]; then
+                exists=TRUE
+            fi
         fi
-    fi
-done
+    done
 
-# if the QID is not present, create a new file
-if [[ $exists == FALSE ]]; then
-    echo "we will create a new file for $input"
-    # query wikidata for label
-    label=$(curl -X "GET" \
-        "https://www.wikidata.org/w/rest.php/wikibase/v0/entities/items/$input/labels" \
-        -H "accept: application/json")
-    # create a name from the label
-    # read a specific label
-    name=$(yq '.en' <<< $label)
-    # make the label conformant with our naming scheme
-    name=$(echo "$name" | perl -pe 's/\W+(\w)/-$1/g and s/([A-Z])/\L$1/g')
-    # create a new, empty yml file
-    yml_path="$output_dir/$name.tool.yml"
-    > $yml_path
-    # write content to this file
-    yq -i ".name = \"$name\"" $yml_path
-    yq -i ".wikidata-id = \"$input\"" $yml_path
+    # if the QID is not present, create a new file
+    if [[ $exists == FALSE ]]; then
+        echo "we will create a new file for $wd_id_input"
+        # query wikidata for label
+        label=$(curl -X "GET" \
+            "https://www.wikidata.org/w/rest.php/wikibase/v0/entities/items/$wd_id_input/labels" \
+            -H "accept: application/json" \
+            --silent)
+        # create a name from the label
+        # read a specific label
+        name=$(yq '.en' <<< $label)
+        # make the label conformant with our naming scheme
+        name=$(echo "$name" | perl -pe 's/\W+(\w)/-$1/g')
+        name=$(echo "$name" | perl -pe 's/([A-Z])/\L$1/g')
+        # create a new, empty yml file
+        yml_path="$output_dir/$name.tool.yml"
+        > $yml_path
+        # write content to this file
+        yq -i ".name = \"$name\"" $yml_path
+        yq -i ".wikidata-id = \"$wd_id_input\"" $yml_path
+    else
+        echo "$wd_id_input already exists in our registry"
+    fi
 else
-    echo "$input already exists in our registry"
+    echo "Input does not contain a Wikidata ID"
 fi
